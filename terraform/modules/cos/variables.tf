94e4bd9264a2cb8d22c38ef29361f33b94ee1da5
Jose C. Massón <jose.masson@canonical.com>
2024-11-26 20:12:23 -0300

    add missing variables

--- terraform/modules/cos/variables.tf
+++ terraform/modules/cos/variables.tf
@@ -25,3 +25,74 @@ variable "minio_password" {
   type        = string
   sensitive   = true
 }
+
+variable "loki_backend_units" {
+  description = "Number of Loki worker units with backend role"
+  type        = number
+  default     = 1
+}
+
+variable "loki_read_units" {
+  description = "Number of Loki worker units with read role"
+  type        = number
+  default     = 1
+}
+
+variable "loki_write_units" {
+  description = "Number of Loki worker units with write roles"
+  type        = number
+  default     = 1
+}
+
+variable "mimir_backend_units" {
+  description = "Number of Mimir worker units with backend role"
+  type        = number
+  default     = 1
+}
+
+variable "mimir_read_units" {
+  description = "Number of Mimir worker units with read role"
+  type        = number
+  default     = 1
+}
+
+variable "mimir_write_units" {
+  description = "Number of Mimir worker units with write role"
+  type        = number
+  default     = 1
+}
+
+variable "tempo_compactor_units" {
+  description = "Number of Tempo worker units with compactor role"
+  type        = number
+  default     = 1
+}
+
+variable "tempo_distributor_units" {
+  description = "Number of Tempo worker units with distributor role"
+  type        = number
+  default     = 1
+}
+
+variable "tempo_ingester_units" {
+  description = "Number of Tempo worker units with ingester role"
+  type        = number
+  default     = 1
+}
+
+variable "tempo_metrics_generator_units" {
+  description = "Number of Tempo worker units with metrics-generator role"
+  type        = number
+  default     = 1
+}
+
+variable "tempo_querier_units" {
+  description = "Number of Tempo worker units with querier role"
+  type        = number
+  default     = 1
+}
+variable "tempo_query_frontend_units" {
+  description = "Number of Tempo worker units with query-frontend role"
+  type        = number
+  default     = 1
+}
