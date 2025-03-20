7793f2edb01bd33615403f1ca71fb2cc2fb8b377
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2025-03-20 17:59:38 +0200

    fix: the self-signed-certificates module now uses "model" instead of "model_name".

--- terraform/modules/cos/main.tf
+++ terraform/modules/cos/main.tf
@@ -50,7 +50,7 @@ module "mimir" {
 module "ssc" {
   count      = var.use_tls ? 1 : 0
   source     = "git::https://github.com/canonical/self-signed-certificates-operator//terraform"
-  model_name = var.model_name
+  model      = var.model_name
   channel    = var.channel
 }
 
