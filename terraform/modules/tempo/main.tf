f63d34470f9cc6e0974c437642f4ec284f5cdf58
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2025-06-12 16:49:47 +0300

    more var changes
    Signed-off-by: Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>

--- terraform/modules/tempo/main.tf
+++ terraform/modules/tempo/main.tf
@@ -1,6 +1,6 @@
 module "tempo_coordinator" {
   source      = "git::https://github.com/canonical/tempo-coordinator-k8s-operator//terraform"
-  model_name  = var.model_name
+  model       = var.model_name
   channel     = var.channel
   units       = var.coordinator_units
   constraints = var.anti_affinity ? "arch=amd64 tags=anti-pod.app.kubernetes.io/name=tempo,anti-pod.topology-key=kubernetes.io/hostname" : null
@@ -9,7 +9,7 @@ module "tempo_coordinator" {
 module "tempo_querier" {
   source      = "git::https://github.com/canonical/tempo-worker-k8s-operator//terraform"
   app_name    = "tempo-querier"
-  model_name  = var.model_name
+  model       = var.model_name
   channel     = var.channel
   constraints = var.anti_affinity ? "arch=amd64 tags=anti-pod.app.kubernetes.io/name=tempo-querier,anti-pod.topology-key=kubernetes.io/hostname" : null
   config = {
@@ -24,7 +24,7 @@ module "tempo_querier" {
 module "tempo_query_frontend" {
   source      = "git::https://github.com/canonical/tempo-worker-k8s-operator//terraform"
   app_name    = "tempo-query-frontend"
-  model_name  = var.model_name
+  model       = var.model_name
   channel     = var.channel
   constraints = var.anti_affinity ? "arch=amd64 tags=anti-pod.app.kubernetes.io/name=tempo-query-frontend,anti-pod.topology-key=kubernetes.io/hostname" : null
   config = {
@@ -39,7 +39,7 @@ module "tempo_query_frontend" {
 module "tempo_ingester" {
   source      = "git::https://github.com/canonical/tempo-worker-k8s-operator//terraform"
   app_name    = "tempo-ingester"
-  model_name  = var.model_name
+  model       = var.model_name
   channel     = var.channel
   constraints = var.anti_affinity ? "arch=amd64 tags=anti-pod.app.kubernetes.io/name=tempo-ingester,anti-pod.topology-key=kubernetes.io/hostname" : null
   config = {
@@ -54,7 +54,7 @@ module "tempo_ingester" {
 module "tempo_distributor" {
   source      = "git::https://github.com/canonical/tempo-worker-k8s-operator//terraform"
   app_name    = "tempo-distributor"
-  model_name  = var.model_name
+  model       = var.model_name
   channel     = var.channel
   constraints = var.anti_affinity ? "arch=amd64 tags=anti-pod.app.kubernetes.io/name=tempo-distributor,anti-pod.topology-key=kubernetes.io/hostname" : null
   config = {
@@ -69,7 +69,7 @@ module "tempo_distributor" {
 module "tempo_compactor" {
   source      = "git::https://github.com/canonical/tempo-worker-k8s-operator//terraform"
   app_name    = "tempo-compactor"
-  model_name  = var.model_name
+  model       = var.model_name
   channel     = var.channel
   constraints = var.anti_affinity ? "arch=amd64 tags=anti-pod.app.kubernetes.io/name=tempo-compactor,anti-pod.topology-key=kubernetes.io/hostname" : null
   config = {
@@ -84,7 +84,7 @@ module "tempo_compactor" {
 module "tempo_metrics_generator" {
   source      = "git::https://github.com/canonical/tempo-worker-k8s-operator//terraform"
   app_name    = "tempo-metrics-generator"
-  model_name  = var.model_name
+  model       = var.model_name
   channel     = var.channel
   constraints = var.anti_affinity ? "arch=amd64 tags=anti-pod.app.kubernetes.io/name=tempo-metrics-generator,anti-pod.topology-key=kubernetes.io/hostname" : null
   config = {
